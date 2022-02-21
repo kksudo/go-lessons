e4a391138228914fbf732d36f6bdbd6bdf594cb9
Kirill <k@kazakov.xyz>
2022-02-22 00:13:58 +0300

    1.1

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
+
 	"golang.org/x/example/stringutil"
 )
 
 var hi = "Hello, OTUS!"
 
 func main() {
-	fmt.Printf("Main direction:%s\n", hi)
-	fmt.Printf("Reverse direction: %s\n", stringutil.Reverse(hi))
+	fmt.Printf("%s", stringutil.Reverse(hi))
 }
