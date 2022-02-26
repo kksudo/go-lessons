e7364e0d0a7ff460e2e8a197169c05f2dae2f6c6
Kirill <k@kazakov.xyz>
2022-02-27 01:57:20 +0300

    HW 2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,80 @@
-package hw02unpackstring
+package main
 
 import (
 	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func isFLetter(s string) error {
+	if !unicode.IsLetter(rune(s[0])) {
+		return ErrInvalidString
+	}
+	return nil
+}
+
+func isOneNum(s rune) error {
+	if unicode.IsDigit(s) {
+		return ErrInvalidString
+	}
+	return nil
+}
+
+func Unpack(s string) (string, error) {
+	// If the curr length is empty, just return
+	if len(s) < 1 {
+		return s, nil
+	}
+	err := isFLetter(s)
+	// OMg how to handle more compact the errors ?
+	if err != nil {
+		return "", err
+	}
+
+	var expStr strings.Builder
+	var accum = ""
+	dig := 0
+
+	for i := 0; i < len(s); i++ {
+		if unicode.IsDigit(rune(s[i])) {
+			err := isOneNum(rune(s[i-1]))
+			if err != nil {
+				return "", err
+			}
+
+			//Convert current val to int
+			dig, _ = strconv.Atoi(string(s[i]))
+			if dig != 0 {
+				expStr.WriteString(strings.Repeat(string(s[i-1]), dig-1))
+			} else {
+				// Flush previous val, if the current number is 0
+				accum = ""
+			}
+		} else {
+			if len(accum) > 0 {
+				expStr.WriteString(accum)
+			}
+			accum = string(s[i])
+		}
+	}
+
+	//Write a last one accumulator val
+	expStr.WriteString(accum)
+	return expStr.String(), err
+}
+
+func main() {
+	str := `a\2s2d3j`
+	res, err := Unpack(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
+
 }
